bc49bc36f95155fec53e51fa2cb2b24bf36324a2
Michael Schienbein <moomba0@gmail.com>
2021-06-22 19:54:45 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const ExpressError = require('./expressError');
+const calcRoutes = require('/routes/calc')
+
+app.use(express.json());
+app.use("/calc", calcRoutes);
+
+
+app.use((req, res, next) => {
+    const e = new ExpressError("Page Not Found", 404);
+    next(e);
+})
+
+app.use((err, req, res, next) => {
+    if (!err.status) err.status = 500;
+    let status = err.status;
+    let msg = err.msg;
+
+    return res.status(err.status).json({
+        error: { msg, status }
+    })
+})
+
+module.exports = app;
---
+++ routes/calc.js
@@ -0,0 +1,25 @@
+app.get('/mean', (req, res, next) => {
+    try {
+
+    } catch (e) {
+
+    }
+})
+
+app.get('/median', (req, res, next) => {
+    try {
+
+    } catch (e) {
+
+    }
+})
+
+app.get('/mode', (req, res, next) => {
+    try {
+
+    } catch (e) {
+
+    }
+})
+
+module.exports = calc;
